7bac78177af540fe0ea397d4c2141235d2682ed1
David Matz <dmatz1@ivytech.edu>
2023-02-21 09:32:43 -0500

    yes

---
+++ app.js
@@ -0,0 +1,28 @@
+//explicet requirement
+const express = require('express');
+
+// expresss app
+const app = express();
+
+// listens for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  // res.send('<p>home page</p>');
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('<p>about page</p>');
+  res.sendFile('./views/about.html', { root: __dirname });
+});
+
+// redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
