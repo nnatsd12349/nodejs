2c0e4347166376464da5f71fc57863aacbc0f824
David Matz <dmatz1@ivytech.edu>
2023-02-21 09:52:11 -0500

    commited and finished tutorial #7

--- app.js
+++ app.js
@@ -11,22 +11,27 @@ app.listen(3000);
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
 
+//info
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
-});
-
-app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>'); //auto html
-  res.sendFile('./views/about.html', { root: __dirname });
-});
-
-// redirected
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-});
-
-// 404 page error
-app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
-});
+    const blogs = [
+      {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: 'Home', blogs });
+  });
+  
+  //redirected
+  app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+  });
+  
+  //created
+  app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+  });
+  
+  // 404 page
+  app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+  });
